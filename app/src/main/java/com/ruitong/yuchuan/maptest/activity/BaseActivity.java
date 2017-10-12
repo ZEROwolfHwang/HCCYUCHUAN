e87774fb8195495537d1b16b6fd2f63701cfb302
ZEROwolf <1059901966@qq.com>
2017-10-12 11:55:53 +0800

    10

--- app/src/main/java/com/ruitong/yuchuan/maptest/activity/BaseActivity.java
+++ app/src/main/java/com/ruitong/yuchuan/maptest/activity/BaseActivity.java
@@ -29,8 +29,7 @@ public  class BaseActivity extends AppCompatActivity{
         setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);//竖屏
 
         if ((getIntent().getFlags() & Intent.FLAG_ACTIVITY_BROUGHT_TO_FRONT) != 0) {
-            // Activity was brought to front and not created,
-            // Thus finishing this will get us to the last viewed activity
+
             finish();
             return;
         }
